32fe3b57ee8fa509e7edfd8a22e160aefb0ab7e8
Sapatsinskiy <Sapatsinskiy63@gmail.com>
2025-01-26 15:29:58 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть перше число:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть друге число:");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {sum}");
+        }
+    }
+}
